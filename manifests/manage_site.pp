cc9d6ff77b205525cb3c7204683e9f9770135201
Drew Wilson <dwilson@opentable.com>
2013-06-07 11:09:28 +0100

    created define to use powershell to ensure required paths exist

--- manifests/manage_site.pp
+++ manifests/manage_site.pp
@@ -1,11 +1,8 @@
 define iis::manage_site($site_path, $host_header, $app_pool, $site_name = $title, $port = '80', $ip_address = '*') {
   include 'iis::param::powershell'
 
-  exec { "CreateSitePath-${site_path}":
-    command   => "${iis::param::powershell::command} -Command \"New-Item -path \\\"${site_path}\\\" -type directory\"",
-    path      => "${iis::param::powershell::path};${::path}",
-    onlyif    => "${iis::param::powershell::command} -Command \"if(Test-Path \\\"${site_path}\\\") { exit 1 } else { exit 0}\"",
-    logoutput => true,
+  iis::createpath { "${site_name}-${site_path}":
+    site_path => $site_path
   }
 
   exec { "CreateSite-${site_name}" :
@@ -13,6 +10,6 @@ define iis::manage_site($site_path, $host_header, $app_pool, $site_name = $title
     path      => "${iis::param::powershell::path};${::path}",
     onlyif    => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; if((Test-Path \"IIS:\\Sites\\${site_name}\")) { exit 1 } else { exit 0 }\"",
     logoutput => true,
-    require   => [ Exec["CreateSitePath-${site_path}"], iis::manage_app_pool[$app_pool] ],
+    require   => [ iis::createpath["${site_name}-${site_path}"], iis::manage_app_pool[$app_pool] ],
   }
 }
