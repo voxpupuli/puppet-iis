1d5ca1d9038aa6d15e987fdbe209c760fc98d936
Drew Wilson <dwilson@opentable.com>
2013-06-13 16:18:14 +0100

    Created define for managing additional site bindings

---
+++ manifests/manage_binding.pp
@@ -0,0 +1,20 @@
+define iis::manage_binding($site_name, $protocol, $port, $host_header, $ip_address = '*') {
+  include 'iis::param::powershell'
+
+  if ! ($protocol in [ 'http', 'https', 'net.tcp', 'net.pipe', 'netmsmq', 'msmq.formatname' ]) {
+    fail('valid protocols \'http\', \'https\', \'net.tcp\', \'net.pipe\', \'netmsmq\', \'msmq.formatname\'')
+  }
+
+  validate_string($site_name)
+  validate_re($site_name,['^(.)+$'], 'site_name must not be empty')
+
+  if ! ($ip_address == '*') {
+    validate_re($ip_address, ['^([0-9]){1,3}\.([0-9]){1,3}\.([0-9]){1,3}\.([0-9]){1,3}$'], "\"${ip_address}\" is not a valid ip address")
+  }
+
+  exec { "ManageBinding-${title}":
+    path    => "${iis::param::powershell::path};${::path}",
+    command => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; New-WebBinding -Name ${site_name} -Port ${port} -Protocol ${protocol} -HostHeader ${host_header} -IPAddress \\\"${ip_address}\\\"\"",
+    onlyif  => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; if (Get-WebBinding -Name ${site_name} -Port ${port} -Protocol ${protocol} -HostHeader ${host_header} -IPAddress \\\"${ip_address}\\\" | Where-Object {\$_.bindingInformation -eq \\\"${ip_address}:${port}:${host_header}\\\"}) { exit 0 } else { exit 1 }\"",
+  }
+}
---
+++ spec/defines/manage_binding_spec.rb
@@ -0,0 +1,75 @@
+require 'spec_helper'
+
+powershell = 'powershell.exe -ExecutionPolicy RemoteSigned'
+
+describe 'iis::manage_binding', :type => :define do
+  describe 'when managing an iis site binding' do
+    let(:title) { 'myWebSite-port-80' }
+    let(:params) { {
+        :site_name   => 'myWebSite',
+        :protocol    => 'http',
+        :host_header => 'myHost.example.com',
+        :port        => '80',
+    } }
+
+    it { should include_class('iis::param::powershell') }
+
+    it { should contain_exec('ManageBinding-myWebSite-port-80').with({
+      'command' => "#{powershell} -Command \"Import-Module WebAdministration; New-WebBinding -Name myWebSite -Port 80 -Protocol http -HostHeader myHost.example.com -IPAddress \\\"*\\\"\"",
+      'onlyif'  => "#{powershell} -Command \"Import-Module WebAdministration; if (Get-WebBinding -Name myWebSite -Port 80 -Protocol http -HostHeader myHost.example.com -IPAddress \\\"*\\\" | Where-Object {\$_.bindingInformation -eq \\\"*:80:myHost.example.com\\\"}) { exit 0 } else { exit 1 }\"",
+    })}
+  end
+
+  describe 'when I pass in a valid ip address' do
+    let(:title) { 'myWebSite-port-80' }
+    let(:params) { {
+        :site_name   => 'myWebSite',
+        :protocol    => 'http',
+        :host_header => 'myHost.example.com',
+        :port        => '80',
+        :ip_address  => '192.168.1.5',
+    } }
+
+    it { should contain_exec('ManageBinding-myWebSite-port-80').with({
+      'command' => "#{powershell} -Command \"Import-Module WebAdministration; New-WebBinding -Name myWebSite -Port 80 -Protocol http -HostHeader myHost.example.com -IPAddress \\\"192.168.1.5\\\"\"",
+      'onlyif'  => "#{powershell} -Command \"Import-Module WebAdministration; if (Get-WebBinding -Name myWebSite -Port 80 -Protocol http -HostHeader myHost.example.com -IPAddress \\\"192.168.1.5\\\" | Where-Object {\$_.bindingInformation -eq \\\"192.168.1.5:80:myHost.example.com\\\"}) { exit 0 } else { exit 1 }\"",
+    })}
+  end
+
+  describe 'when I pass in an invalid ip address' do
+    let(:title) { 'myWebSite-port-80' }
+    let(:params) { {
+        :site_name   => 'myWebSite',
+        :protocol    => 'http',
+        :host_header => 'myHost.example.com',
+        :port        => '80',
+        :ip_address  => 'this is not an address',
+    } }
+
+    it { expect { should contain_exec('ManageBinding-myWebSite-port-80') }.to raise_error(Puppet::Error, /"this is not an address" is not a valid ip address/) }
+  end
+
+  describe 'when I pass in an empty site_name' do
+    let(:title) { 'myWebSite-port-80' }
+    let(:params) { {
+        :site_name   => '',
+        :protocol    => 'http',
+        :host_header => 'myHost.example.com',
+        :port        => '80',
+    } }
+
+    it { expect { should contain_exec('ManageBinding-myWebSite-port-80') }.to raise_error(Puppet::Error, /site_name must not be empty/) }
+  end
+
+  describe 'when protocol is not valid' do
+    let(:title) { 'myWebSite-port-80' }
+    let(:params) { {
+        :site_name   => 'myWebSite',
+        :protocol    => 'banana',
+        :host_header => 'myHost.example.com',
+        :port        => '80',
+    } }
+
+    it { expect { should contain_exec('ManageBinding-myWebSite-port-80') }.to raise_error(Puppet::Error, /valid protocols 'http', 'https', 'net.tcp', 'net.pipe', 'netmsmq', 'msmq.formatname'/) }
+  end
+end
