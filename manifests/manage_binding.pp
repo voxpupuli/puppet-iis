ab3a22265d5e3cb72e03da2dc502249d6799cc6b
Drew Wilson <dwilson@opentable.com>
2013-06-14 11:58:44 +0100

    Added require manage_site to manage_binding define

--- manifests/manage_binding.pp
+++ manifests/manage_binding.pp
@@ -17,6 +17,7 @@ define iis::manage_binding($site_name, $protocol, $port, $host_header, $ip_addre
     command   => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; New-WebBinding -Name ${site_name} -Port ${port} -Protocol ${protocol} -HostHeader ${host_header} -IPAddress \\\"${ip_address}\\\"\"",
     onlyif    => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; if (Get-WebBinding -Name ${site_name} -Port ${port} -Protocol ${protocol} -HostHeader ${host_header} -IPAddress \\\"${ip_address}\\\" | Where-Object {\$_.bindingInformation -eq \\\"${ip_address}:${port}:${host_header}\\\"}) { exit 1 } else { exit 0 }\"",
     logoutput => true,
+    require   => Iis::Manage_site[$site_name],
   }
 }
 
