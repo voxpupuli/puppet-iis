9e5b7bf6df097085fcc0ebc3b53864f2897836b5
Drew Wilson <dwilson@opentable.com>
2013-06-28 10:04:07 +0100

    check if createpath resource has already been defined for a path

--- manifests/createpath.pp
+++ manifests/createpath.pp
@@ -5,10 +5,12 @@ define iis::createpath($site_path = undef) {
     fail('site_path is undefined')
   }
 
-  exec { $title:
-    command   => "${iis::param::powershell::command} -Command \"New-Item -path \\\"${site_path}\\\" -type directory\"",
-    path      => "${iis::param::powershell::path};${::path}",
-    onlyif    => "${iis::param::powershell::command} -Command \"if(Test-Path \\\"${site_path}\\\") { exit 1 } else { exit 0 }\"",
-    logoutput => true,
+  if ! defined(Exec["Create-Path-${title}"]) {
+    exec { "Create-Path-${title}":
+      command   => "${iis::param::powershell::command} -Command \"New-Item -path \\\"${site_path}\\\" -type directory\"",
+      path      => "${iis::param::powershell::path};${::path}",
+      onlyif    => "${iis::param::powershell::command} -Command \"if(Test-Path \\\"${site_path}\\\") { exit 1 } else { exit 0 }\"",
+      logoutput => true,
+    }
   }
 }
