844e21a77eb3a5cf72390969ed96e194d1deaf0a
Bryce Catlin <bcatlin@opentable.com>
2013-07-05 12:43:23 -0700

    Adding default $host_header, fixing bug where empty host header didn't work.

--- manifests/manage_site.pp
+++ manifests/manage_site.pp
@@ -1,12 +1,17 @@
-define iis::manage_site($site_path, $host_header, $app_pool, $site_name = $title, $port = '80', $ip_address = '*') {
+define iis::manage_site($site_path, $host_header = '', $app_pool, $site_name = $title, $port = '80', $ip_address = '*') {
   include 'iis::param::powershell'
 
   iis::createpath { "${site_name}-${site_path}":
     site_path => $site_path
   }
+  
+  $host_header_string = ''
+  if $host_header != '' {
+    $host_header_string = "-HostHeader ${host_header}"
+  }
 
   exec { "CreateSite-${site_name}" :
-    command   => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; New-WebSite -Name ${site_name} -Port ${port} -IP ${ip_address} -HostHeader ${host_header} -PhysicalPath ${site_path} -ApplicationPool ${app_pool}\"",
+    command   => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; New-WebSite -Name ${site_name} -Port ${port} -IP ${ip_address} ${host_header_string} -PhysicalPath ${site_path} -ApplicationPool ${app_pool}\"",
     path      => "${iis::param::powershell::path};${::path}",
     onlyif    => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; if((Test-Path \"IIS:\\Sites\\${site_name}\")) { exit 1 } else { exit 0 }\"",
     logoutput => true,
