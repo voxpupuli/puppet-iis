672a31d7bd79b471e4f25a9a7889eac981da096c
Bryce Catlin <bcatlin@opentable.com>
2013-10-14 17:06:31 -0700

    Added ensure param, default to 'present', implemented 'absent', added tests.

--- spec/defines/manage_virtual_application_spec.rb
+++ spec/defines/manage_virtual_application_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 powershell = 'powershell.exe -ExecutionPolicy RemoteSigned'
 
 describe 'iis::manage_virtual_application', :type => :define do
-  describe 'when I create a virtual application in IIS' do
-    let(:title) { 'reviews' }
+  describe 'when I create a virtual application in IIS using default params' do
+    let(:title) { 'mySite' }
     let(:params) {{
         :site_name => 'myWebSite',
         :site_path => 'C:\inetpub\wwwroot\myHost',
@@ -13,9 +13,69 @@ describe 'iis::manage_virtual_application', :type => :define do
 
     it { should include_class('iis::param::powershell') }
 
-    it { should contain_exec('CreateVirtualApplication-myWebSite-reviews').with({
-      'command' => "#{powershell} -Command \"Import-Module WebAdministration; New-WebApplication -Name reviews -Site myWebSite -PhysicalPath C:\\inetpub\\wwwroot\\myHost -ApplicationPool myAppPool.example.com\"",
-      'onlyif'  => "#{powershell} -Command \"Import-Module WebAdministration; if((Test-Path \"IIS:\\Sites\\myWebSite\\reviews\")) { exit 1 } else { exit 0 }\"",
+    it { should contain_exec('CreateVirtualApplication-myWebSite-mySite').with({
+      'command' => "#{powershell} -Command \"Import-Module WebAdministration; New-WebApplication -Name mySite -Site myWebSite -PhysicalPath C:\\inetpub\\wwwroot\\myHost -ApplicationPool myAppPool.example.com\"",
+      'onlyif'  => "#{powershell} -Command \"Import-Module WebAdministration; if((Test-Path \"IIS:\\Sites\\myWebSite\\mySite\")) { exit 1 } else { exit 0 }\"",
+    })}
+  end
+
+  describe 'when I create a virtual application in IIS and set ensure to present' do
+    let(:title) { 'mySite' }
+    let(:params) {{
+        :site_name => 'myWebSite',
+        :site_path => 'C:\inetpub\wwwroot\myHost',
+        :app_pool => 'myAppPool.example.com',
+        :ensure => 'present'
+    }}
+
+    it { should contain_exec('CreateVirtualApplication-myWebSite-mySite').with({
+      'command' => "#{powershell} -Command \"Import-Module WebAdministration; New-WebApplication -Name mySite -Site myWebSite -PhysicalPath C:\\inetpub\\wwwroot\\myHost -ApplicationPool myAppPool.example.com\"",
+      'onlyif'  => "#{powershell} -Command \"Import-Module WebAdministration; if((Test-Path \"IIS:\\Sites\\myWebSite\\mySite\")) { exit 1 } else { exit 0 }\"",
+    })}
+  end
+
+  describe 'when I create a virtual application in IIS and set ensure to installed' do
+    let(:title) { 'mySite' }
+    let(:params) {{
+        :site_name => 'myWebSite',
+        :site_path => 'C:\inetpub\wwwroot\myHost',
+        :app_pool => 'myAppPool.example.com',
+        :ensure => 'installed'
+    }}
+
+    it { should contain_exec('CreateVirtualApplication-myWebSite-mySite').with({
+      'command' => "#{powershell} -Command \"Import-Module WebAdministration; New-WebApplication -Name mySite -Site myWebSite -PhysicalPath C:\\inetpub\\wwwroot\\myHost -ApplicationPool myAppPool.example.com\"",
+      'onlyif'  => "#{powershell} -Command \"Import-Module WebAdministration; if((Test-Path \"IIS:\\Sites\\myWebSite\\mySite\")) { exit 1 } else { exit 0 }\"",
+    })}
+  end
+
+  describe 'when I create a virtual application in IIS and set ensure to purged' do
+    let(:title) { 'mySite' }
+    let(:params) {{
+        :site_name => 'myWebSite',
+        :site_path => 'C:\inetpub\wwwroot\myHost',
+        :app_pool => 'myAppPool.example.com',
+        :ensure => 'purged'
+    }}
+
+    it { should contain_exec('DeleteVirtualApplication-myWebSite-mySite').with({
+      'command' => "#{powershell} -Command \"Import-Module WebAdministration; Remove-WebApplication -Name mySite -Site myWebSite\"",
+      'onlyif'  => "#{powershell} -Command \"Import-Module WebAdministration; if(!(Test-Path \"IIS:\\Sites\\myWebSite\\mySite\")) { exit 1 } else { exit 0 }\"",
+    })}
+  end
+
+  describe 'when I create a virtual application in IIS and set ensure to absent' do
+    let(:title) { 'mySite' }
+    let(:params) {{
+        :site_name => 'myWebSite',
+        :site_path => 'C:\inetpub\wwwroot\myHost',
+        :app_pool => 'myAppPool.example.com',
+        :ensure => 'absent'
+    }}
+
+    it { should contain_exec('DeleteVirtualApplication-myWebSite-mySite').with({
+      'command' => "#{powershell} -Command \"Import-Module WebAdministration; Remove-WebApplication -Name mySite -Site myWebSite\"",
+      'onlyif'  => "#{powershell} -Command \"Import-Module WebAdministration; if(!(Test-Path \"IIS:\\Sites\\myWebSite\\mySite\")) { exit 1 } else { exit 0 }\"",
     })}
   end
 end
