21e78b1960404a46f1cd3a53eaac23f8c9cb354d
Bryce Catlin <bcatlin@opentable.com>
2013-10-14 17:29:27 -0700

    Added ability to delete websites, and tests to validate.

--- manifests/manage_site.pp
+++ manifests/manage_site.pp
@@ -1,15 +1,26 @@
-define iis::manage_site($site_path, $app_pool, $host_header = '', $site_name = $title, $port = '80', $ip_address = '*') {
+define iis::manage_site($site_path, $app_pool, $host_header = '', $site_name = $title, $port = '80', $ip_address = '*', $ensure = 'present') {
   include 'iis::param::powershell'
 
-  iis::createpath { "${site_name}-${site_path}":
-    site_path => $site_path
-  }
+  validate_re($ensure, '^(present|installed|absent|purged)$', 'ensure must be one of \'present\', \'installed\', \'absent\', \'purged\'')
 
-  exec { "CreateSite-${site_name}" :
-    command   => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; New-WebSite -Name \\\"${site_name}\\\" -Port ${port} -IP ${ip_address} -HostHeader \\\"${host_header}\\\" -PhysicalPath \\\"${site_path}\\\" -ApplicationPool \\\"${app_pool}\\\"\"",
-    path      => "${iis::param::powershell::path};${::path}",
-    onlyif    => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; if((Test-Path \"IIS:\\Sites\\${site_name}\")) { exit 1 } else { exit 0 }\"",
-    logoutput => true,
-    require   => [ Iis::Createpath["${site_name}-${site_path}"], Iis::Manage_app_pool[$app_pool] ],
+  if ($ensure in ['present','installed']) {
+    iis::createpath { "${site_name}-${site_path}":
+      site_path => $site_path
+    }
+
+    exec { "CreateSite-${site_name}" :
+      command   => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; New-WebSite -Name \\\"${site_name}\\\" -Port ${port} -IP ${ip_address} -HostHeader \\\"${host_header}\\\" -PhysicalPath \\\"${site_path}\\\" -ApplicationPool \\\"${app_pool}\\\"\"",
+      path      => "${iis::param::powershell::path};${::path}",
+      onlyif    => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; if((Test-Path \"IIS:\\Sites\\${site_name}\")) { exit 1 } else { exit 0 }\"",
+      logoutput => true,
+      require   => [ Iis::Createpath["${site_name}-${site_path}"], Iis::Manage_app_pool[$app_pool] ],
+    }
+  } else {
+    exec { "DeleteSite-${site_name}" :
+      command   => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; Remove-WebSite -Name \\\"${site_name}\\\" \"",
+      path      => "${iis::param::powershell::path};${::path}",
+      onlyif    => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; if(!(Test-Path \"IIS:\\Sites\\${site_name}\")) { exit 1 } else { exit 0 }\"",
+      logoutput => true,
+    }
   }
 }
--- spec/defines/manage_site_spec.rb
+++ spec/defines/manage_site_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 powershell = 'powershell.exe -ExecutionPolicy RemoteSigned'
 
 describe 'iis::manage_site', :type => :define do
-  describe 'when managing the iis site' do
+  describe 'when managing the iis site using default params' do
     let(:title) { 'myWebSite' }
     let(:params) { {
         :app_pool    => 'myAppPool.example.com',
@@ -27,6 +27,7 @@ describe 'iis::manage_site', :type => :define do
         :site_path   => 'C:\inetpub\wwwroot\path',
         :port        => '1080',
         :ip_address  => '127.0.0.1',
+        :ensure      => 'present',
     }}
 
     it { should include_class('iis::param::powershell') }
@@ -34,6 +35,66 @@ describe 'iis::manage_site', :type => :define do
     it { should contain_exec('CreateSite-myWebSite').with({
       'command' => "#{powershell} -Command \"Import-Module WebAdministration; New-WebSite -Name \\\"myWebSite\\\" -Port 1080 -IP 127.0.0.1 -HostHeader \\\"myHost.example.com\\\" -PhysicalPath \\\"C:\\inetpub\\wwwroot\\path\\\" -ApplicationPool \\\"myAppPool.example.com\\\"\"",
       'onlyif'  => "#{powershell} -Command \"Import-Module WebAdministration; if((Test-Path \"IIS:\\Sites\\myWebSite\")) { exit 1 } else { exit 0 }\"",
+    })}
+  end
+
+  describe 'when managing the iis site and setting ensure to present' do
+    let(:title) { 'myWebSite' }
+    let(:params) { {
+        :app_pool    => 'myAppPool.example.com',
+        :host_header => 'myHost.example.com',
+        :site_path   => 'C:\inetpub\wwwroot\myWebSite',
+        :ensure      => 'present',
+    } }
+
+    it { should contain_exec('CreateSite-myWebSite').with({
+      'command' => "#{powershell} -Command \"Import-Module WebAdministration; New-WebSite -Name \\\"myWebSite\\\" -Port 80 -IP * -HostHeader \\\"myHost.example.com\\\" -PhysicalPath \\\"C:\\inetpub\\wwwroot\\myWebSite\\\" -ApplicationPool \\\"myAppPool.example.com\\\"\"",
+      'onlyif'  => "#{powershell} -Command \"Import-Module WebAdministration; if((Test-Path \"IIS:\\Sites\\myWebSite\")) { exit 1 } else { exit 0 }\"",
+    })}
+  end
+
+  describe 'when managing the iis site and setting ensure to installed' do
+    let(:title) { 'myWebSite' }
+    let(:params) { {
+        :app_pool    => 'myAppPool.example.com',
+        :host_header => 'myHost.example.com',
+        :site_path   => 'C:\inetpub\wwwroot\myWebSite',
+        :ensure      => 'installed',
+    } }
+
+    it { should contain_exec('CreateSite-myWebSite').with({
+      'command' => "#{powershell} -Command \"Import-Module WebAdministration; New-WebSite -Name \\\"myWebSite\\\" -Port 80 -IP * -HostHeader \\\"myHost.example.com\\\" -PhysicalPath \\\"C:\\inetpub\\wwwroot\\myWebSite\\\" -ApplicationPool \\\"myAppPool.example.com\\\"\"",
+      'onlyif'  => "#{powershell} -Command \"Import-Module WebAdministration; if((Test-Path \"IIS:\\Sites\\myWebSite\")) { exit 1 } else { exit 0 }\"",
+    })}
+  end
+
+  describe 'when managing the iis site and setting ensure to absent' do
+    let(:title) { 'myWebSite' }
+    let(:params) { {
+        :app_pool    => 'myAppPool.example.com',
+        :host_header => 'myHost.example.com',
+        :site_path   => 'C:\inetpub\wwwroot\myWebSite',
+        :ensure      => 'absent',
+    } }
+
+    it { should contain_exec('DeleteSite-myWebSite').with({
+      'command' => "#{powershell} -Command \"Import-Module WebAdministration; Remove-WebSite -Name \\\"myWebSite\\\"\"",
+      'onlyif'  => "#{powershell} -Command \"Import-Module WebAdministration; if(!(Test-Path \"IIS:\\Sites\\myWebSite\")) { exit 1 } else { exit 0 }\"",
+    })}
+  end
+
+  describe 'when managing the iis site and setting ensure to purged' do
+    let(:title) { 'myWebSite' }
+    let(:params) { {
+        :app_pool    => 'myAppPool.example.com',
+        :host_header => 'myHost.example.com',
+        :site_path   => 'C:\inetpub\wwwroot\myWebSite',
+        :ensure      => 'purged',
+    } }
+
+    it { should contain_exec('DeleteSite-myWebSite').with({
+      'command' => "#{powershell} -Command \"Import-Module WebAdministration; Remove-WebSite -Name \\\"myWebSite\\\"\"",
+      'onlyif'  => "#{powershell} -Command \"Import-Module WebAdministration; if(!(Test-Path \"IIS:\\Sites\\myWebSite\")) { exit 1 } else { exit 0 }\"",
     })}
   end
 end
