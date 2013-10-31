e5b19224c88cc4590f2bca109b31c1f4bea33549
Drew Wilson <dwilson@opentable.com>
2013-10-31 14:24:20 -0700

    Bugfix: corrected quotes escaping around App pool names, fixed capitalisation for required resources

--- manifests/manage_virtual_application.pp
+++ manifests/manage_virtual_application.pp
@@ -11,15 +11,15 @@ define iis::manage_virtual_application($site_name, $site_path, $app_pool, $virtu
     exec { "CreateVirtualApplication-${site_name}-${virtual_application_name}" :
       command   => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; New-WebApplication -Name ${virtual_application_name} -Site ${site_name} -PhysicalPath ${site_path} -ApplicationPool ${app_pool}\"",
       path      => "${iis::param::powershell::path};${::path}",
-      onlyif    => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; if((Test-Path \"IIS:\\Sites\\${site_name}\\${virtual_application_name}\")) { exit 1 } else { exit 0 }\"",
-      require   => [ iis::createpath["${site_name}-${virtual_application_name}-${site_path}"], iis::manage_site[$site_name] ],
+      onlyif    => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; if((Test-Path \\\"IIS:\\Sites\\${site_name}\\${virtual_application_name}\\\")) { exit 1 } else { exit 0 }\"",
+      require   => [ Iis::Createpath["${site_name}-${virtual_application_name}-${site_path}"], Iis::Manage_site[$site_name] ],
       logoutput => true,
     }
   } else {
     exec { "DeleteVirtualApplication-${site_name}-${virtual_application_name}" :
       command   => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; Remove-WebApplication -Name ${virtual_application_name} -Site ${site_name}\"",
       path      => "${iis::param::powershell::path};${::path}",
-      onlyif    => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; if(!(Test-Path \"IIS:\\Sites\\${site_name}\\${virtual_application_name}\")) { exit 1 } else { exit 0 }\"",
+      onlyif    => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; if(!(Test-Path \\\"IIS:\\Sites\\${site_name}\\${virtual_application_name}\\\")) { exit 1 } else { exit 0 }\"",
       logoutput => true,
     }
   }
