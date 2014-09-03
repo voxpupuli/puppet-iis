4e20116be36c7ed17b6585fc95d19a6cdbdc7108
Jérôme RIVIERE <jerome.riviere974@gmail.com>
2014-09-03 15:34:10 +0200

    Some updates
    Change joshcooper powershell module to puppetlabs powershell module
    Remove warning when set bindings
    Add flag for choosing if we want to manage bindings without managing
    sites

--- manifests/manage_binding.pp
+++ manifests/manage_binding.pp
@@ -1,5 +1,5 @@
 #
-define iis::manage_binding($site_name, $protocol, $port, $host_header = '', $ip_address = '*', $certificate_thumbprint = '', $ensure = 'present') {
+define iis::manage_binding($site_name, $protocol, $port, $host_header = '', $ip_address = '*', $certificate_thumbprint = '', $ensure = 'present', $only_manage_binding = false) {
   include 'iis::param::powershell'
 
   if ! ($protocol in [ 'http', 'https', 'net.tcp', 'net.pipe', 'netmsmq', 'msmq.formatname' ]) {
@@ -9,20 +9,28 @@ define iis::manage_binding($site_name, $protocol, $port, $host_header = '', $ip_
   validate_string($site_name)
   validate_re($site_name,['^(.)+$'], 'site_name must not be empty')
   validate_re($ensure, '^(present|installed|absent|purged)$', 'ensure must be one of \'present\', \'installed\', \'absent\', \'purged\'')
-
+  validate_bool($only_manage_binding)
   if ! ($ip_address == '*') {
     validate_re($ip_address, ['^([0-9]){1,3}\.([0-9]){1,3}\.([0-9]){1,3}\.([0-9]){1,3}$'], "\"${ip_address}\" is not a valid ip address")
   }
 
   if ($ensure in ['present','installed']) {
-    exec { "CreateBinding-${title}":
-      path      => "${iis::param::powershell::path};${::path}",
-      command   => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; New-WebBinding -Name \\\"${site_name}\\\" -Port ${port} -Protocol \\\"${protocol}\\\" -HostHeader \\\"${host_header}\\\" -IPAddress \\\"${ip_address}\\\"\"",
-      onlyif    => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; if (Get-WebBinding -Name \\\"${site_name}\\\" -Port ${port} -Protocol \\\"${protocol}\\\" -HostHeader \\\"${host_header}\\\" -IPAddress \\\"${ip_address}\\\" | Where-Object {\$_.bindingInformation -eq \\\"${ip_address}:${port}:${host_header}\\\"}) { exit 1 } else { exit 0 }\"",
-      logoutput => true,
-      require   => Iis::Manage_site[$site_name],
+    if($only_manage_binding){
+      exec { "CreateBinding-${title}":
+        path      => "${iis::param::powershell::path};${::path}",
+        command   => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; New-WebBinding -Name \\\"${site_name}\\\" -Port ${port} -Protocol \\\"${protocol}\\\" -HostHeader \\\"${host_header}\\\" -IPAddress \\\"${ip_address}\\\"\"",
+        onlyif    => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; if (Get-WebBinding -Name \\\"${site_name}\\\" -Port ${port} -Protocol \\\"${protocol}\\\" -HostHeader \\\"${host_header}\\\" -IPAddress \\\"${ip_address}\\\" | Where-Object {\$_.bindingInformation -eq \\\"${ip_address}:${port}:${host_header}\\\"}) { exit 1 } else { exit 0 }\"",
+        logoutput => true,
+      }
+    }else{
+        exec { "CreateBinding-${title}":
+        path      => "${iis::param::powershell::path};${::path}",
+        command   => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; New-WebBinding -Name \\\"${site_name}\\\" -Port ${port} -Protocol \\\"${protocol}\\\" -HostHeader \\\"${host_header}\\\" -IPAddress \\\"${ip_address}\\\"\"",
+        onlyif    => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; if (Get-WebBinding -Name \\\"${site_name}\\\" -Port ${port} -Protocol \\\"${protocol}\\\" -HostHeader \\\"${host_header}\\\" -IPAddress \\\"${ip_address}\\\" | Where-Object {\$_.bindingInformation -eq \\\"${ip_address}:${port}:${host_header}\\\"}) { exit 1 } else { exit 0 }\"",
+        logoutput => true,
+        require   => Iis::Manage_site[$site_name],
+      }
     }
-
     if ($protocol == 'https') {
       validate_re($certificate_thumbprint, ['^(.)+$'], 'certificate_thumbprint required for https bindings')
       if ($ip_address == '0.0.0.0') {
