70d3329c9920eb4bf89bb15b263ca1c51bd08a4e
Steve Maddison <steve@cosam.org>
2014-10-31 16:28:01 +0100

    Support for configuration of queue length, max worker processes and recycling time interval of Application Pool.

--- manifests/manage_app_pool.pp
+++ manifests/manage_app_pool.pp
@@ -1,10 +1,22 @@
 #
-define iis::manage_app_pool($app_pool_name = $title, $enable_32_bit = false, $managed_runtime_version = 'v4.0', $managed_pipeline_mode = 'Integrated', $ensure = 'present') {
+define iis::manage_app_pool(
+  $ensure = 'present',
+  $app_pool_name = $title,
+  $enable_32_bit = false,
+  $managed_runtime_version = 'v4.0',
+  $managed_pipeline_mode = 'Integrated',
+  $queue_length = 1000,
+  $max_worker_processes = 1,
+  $recycling_time_interval = 1740,
+) {
 
   validate_bool($enable_32_bit)
   validate_re($managed_runtime_version, ['^(v2\.0|v4\.0)$'])
   validate_re($managed_pipeline_mode, ['^(Integrated|Classic)$'])
   validate_re($ensure, '^(present|installed|absent|purged)$', 'ensure must be one of \'present\', \'installed\', \'absent\', \'purged\'')
+  validate_re("${queue_length}", '^[0-9]+$', 'queue_length must be a positive integer')
+  validate_re("${max_worker_processes}", '^[0-9]+$', 'max_worker_processes must be a positive integer')
+  validate_re("${recycling_time_interval}", '^[0-9]+$', 'recycling_time_interval must be a positive integer')
 
   include 'param::powershell'
 
@@ -36,6 +48,30 @@ define iis::manage_app_pool($app_pool_name = $title, $enable_32_bit = false, $ma
       path      => "${iis::param::powershell::path};${::path}",
       command   => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; Set-ItemProperty \\\"IIS:\\AppPools\\${app_pool_name}\\\" managedPipelineMode ${managed_pipeline_mode}\"",
       onlyif    => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; if((Get-ItemProperty \\\"IIS:\\AppPools\\${app_pool_name}\\\" managedPipelineMode).Value.CompareTo('${managed_pipeline_mode}') -eq 0) { exit 1 } else { exit 0 }\"",
+      require   => Exec["Create-${app_pool_name}"],
+      logoutput => true,
+    }
+
+    exec { "QueueLength-${app_pool_name}" :
+      path      => "${iis::param::powershell::path};${::path}",
+      command   => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; Set-ItemProperty \\\"IIS:\\AppPools\\${app_pool_name}\\\" queueLength ${queue_length}\"",
+      onlyif    => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; if((Get-ItemProperty \\\"IIS:\\AppPools\\${app_pool_name}\\\" queueLength).Value -eq ${queue_length}) { exit 1 } else { exit 0 }\"",
+      require   => Exec["Create-${app_pool_name}"],
+      logoutput => true,
+    }
+
+    exec { "MaxWorkerProcesses-${app_pool_name}" :
+      path      => "${iis::param::powershell::path};${::path}",
+      command   => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; Set-ItemProperty \\\"IIS:\\AppPools\\${app_pool_name}\\\" processModel.maxProcesses ${max_worker_processes}\"",
+      onlyif    => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; if((Get-ItemProperty \\\"IIS:\\AppPools\\${app_pool_name}\\\" processModel.maxProcesses).Value -eq ${max_worker_processes}) { exit 1 } else { exit 0 }\"",
+      require   => Exec["Create-${app_pool_name}"],
+      logoutput => true,
+    }
+
+    exec { "RecyclingTimeInterval-${app_pool_name}" :
+      path      => "${iis::param::powershell::path};${::path}",
+      command   => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; Set-ItemProperty \\\"IIS:\\AppPools\\${app_pool_name}\\\" recycling.periodicRestart.time (New-TimeSpan -Minutes ${recycling_time_interval})\"",
+      onlyif    => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; if((Get-ItemProperty \\\"IIS:\\AppPools\\${app_pool_name}\\\" recycling.periodicRestart.time.Value).TotalMinutes -eq ${recycling_time_interval}) { exit 1 } else { exit 0 }\"",
       require   => Exec["Create-${app_pool_name}"],
       logoutput => true,
     }
