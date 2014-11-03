1581ecf00f40cca45145957e326e3d13dc691a48
Abhishek Dastidar <abhishek.dastidar@qumu.com>
2014-11-03 13:19:08 +0000

    use powershell provider

--- manifests/manage_virtual_directory.pp
+++ manifests/manage_virtual_directory.pp
@@ -1,18 +1,17 @@
 define iis::manage_virtual_directory($site_name, $directory, $path) {
-  include 'iis::param::powershell'
 
     exec { "WebConfiguration-create-${site_name}-${directory}" :
-      command   => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; New-Item 'IIS:\Sites\\${site_name}\\${directory}' -type VirtualDirectory -PhysicalPath ${path} \"",
-      path      => "${iis::param::powershell::path};${::path}",
-      onlyif    => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; if(Test-Path -Path 'IIS:\Sites\\${site_name}\\${directory}') { exit 1 } else { exit 0 } \"",
+      command   => "Import-Module WebAdministration; New-Item 'IIS:\Sites\\${site_name}\\${directory}' -type VirtualDirectory -PhysicalPath ${path} ",
+      onlyif    => "Import-Module WebAdministration; if(Test-Path -Path 'IIS:\Sites\\${site_name}\\${directory}') { exit 1 } else { exit 0 } ",
       logoutput => true,
+      provider  => powershell
     }
 
     exec { "WebConfiguration-edit-${site_name}-${directory}" :
-      command   => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; Set-ItemProperty 'IIS:\Sites\\${site_name}\\${directory}' -Name PhysicalPath -Value '${path}' \"",
-      path      => "${iis::param::powershell::path};${::path}",
-      onlyif    => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; if((Get-ItemProperty 'IIS:\Sites\\${site_name}\\${directory}' -Name PhysicalPath).value -eq '${path}') { exit 1 } else { exit 0 }\"",
+      command   => "Import-Module WebAdministration; Set-ItemProperty 'IIS:\Sites\\${site_name}\\${directory}' -Name PhysicalPath -Value '${path}' ",
+      onlyif    => "Import-Module WebAdministration; if((Get-ItemProperty 'IIS:\Sites\\${site_name}\\${directory}' -Name PhysicalPath).value -eq '${path}') { exit 1 } else { exit 0 }",
       logoutput => true,
+      provider  => powershell,
       require   => Exec["WebConfiguration-create-${site_name}-${directory}"]
     }
 }
