6a7728f0b6f72e43b57d5a5b2981e6363aacc985
WhatsARanjit <whatsaranjit@gmail.com>
2015-03-04 06:02:25 -0500

    (maint) iis_site rewrite
    - type prefetch done
    
    - Added functions to set protocol, port, and IP together
    
    - Apparently sslFlags=0 means SSL is on
    
    - Added host_header functionality
    
    - Added state to site for Started or Stopped
    
    - Added false for empty host_header
    
    - Added refresh event to iis_site

---
+++ lib/puppet/provider/iis_site/powershell.rb
@@ -0,0 +1,194 @@
+require 'puppet/provider/iispowershell'
+require 'json'
+
+Puppet::Type.type(:iis_site).provide(:powershell, :parent => Puppet::Provider::Iispowershell) do
+
+  def initialize(value={})
+    super(value)
+    @property_flush = {
+      'itemproperty' => {},
+      'binders'      => {},
+    }
+  end
+
+  def self.iisnames
+    {
+      :name        => 'name',
+      :path        => 'physicalPath',
+      :app_pool    => 'applicationPool',
+    }
+  end
+
+  def self.instances
+    inst_cmd = <<-ps1
+Import-Module WebAdministration;
+gci "IIS:\\sites" | %{ Get-ItemProperty $_.PSPath  | Select name, PhysicalPath, ApplicationPool, HostHeader, State, Bindings } | ConvertTo-Json
+ps1
+    site_json = JSON.parse(run(inst_cmd))
+    # The command returns a Hash if there is 1 site
+    site_json = [site_json] if site_json.is_a?(Hash)
+    site_json.collect do |site|
+      site_hash               = {}
+      site_hash[:ensure]      = site['state'].downcase
+      site_hash[:name]        = site['name']
+      site_hash[:path]        = site['physicalPath']
+      site_hash[:app_pool]    = site['applicationPool']
+      bindings                = site['bindings']['Collection'].first['bindingInformation']
+      site_hash[:protocol]    = site['bindings']['Collection'].first['protocol']
+      site_hash[:ip]          = bindings.split(':')[0]
+      site_hash[:port]        = bindings.split(':')[1]
+      site_hash[:host_header] = bindings.split(':')[2]
+      if site['bindings']['Collection'].first['sslFlags'] == 0
+        site_hash[:ssl]       = :true
+      else
+        site_hash[:ssl]       = :false
+      end
+      new(site_hash)
+    end
+  end
+
+  def self.prefetch(resources)
+    sites = instances
+    resources.keys.each do |site|
+      if provider = sites.find{ |s| s.name == site }
+        resources[site].provider = provider
+      end
+    end
+  end
+
+  def exists?
+    [ 'stopped', 'started' ].include?(@property_hash[:ensure])
+  end
+
+  mk_resource_methods
+
+  def create
+    createSwitches = [
+      "-Name \"#{@resource[:name]}\"",
+      "-Port #{@resource[:port]} -IP #{@resource[:ip]}",
+      "-HostHeader \"#{@resource[:host_header]}\"",
+      "-PhysicalPath \"#{@resource[:path]}\"",
+      "-ApplicationPool \"#{@resource[:app_pool]}\"",
+      "-Ssl:$#{@resource[:ssl]}",
+      '-Force'
+    ]
+    inst_cmd = "Import-Module WebAdministration; New-Website #{createSwitches.join(' ')}"
+    resp = Puppet::Type::Iis_site::ProviderPowershell.run(inst_cmd)
+
+    @resource.original_parameters.each_key do |k|
+      @property_hash[k] = @resource[k]
+    end
+    @property_hash[:ensure]      = :present unless @property_hash[:ensure]
+    @property_hash[:port]        = @resource[:port]
+    @property_hash[:ip]          = @resource[:ip]
+    @property_hash[:host_header] = @resource[:host_header]
+    @property_hash[:path]        = @resource[:path]
+    @property_hash[:ssl]         = @resource[:ssl]
+
+    exists? ? (return true) : (return false)
+  end
+
+  def destroy
+    inst_cmd = "Import-Module WebAdministration; Remove-Website -Name \"#{@property_hash[:name]}\""
+    resp = Puppet::Type::Iis_site::ProviderPowershell.run(inst_cmd)
+    fail(resp) if resp.length > 0
+    @property_hash.clear
+    exists? ? (return false) : (return true)
+  end
+
+  iisnames.each do |property,iisname|
+    next if property == :ensure
+    define_method "#{property.to_s}=" do |value|
+      @property_flush['itemproperty'][iisname] = value
+      @property_hash[property.to_sym] = value
+    end
+  end
+
+  # These three properties have to be submitted together
+  def self.binders
+    [
+      'protocol',
+      'ip',
+      'port',
+      'host_header',
+      'ssl'
+    ]
+  end
+
+  binders.each do |property|
+    define_method "#{property}=" do |value|
+      @property_flush['binders'][property] = value
+      @property_hash[property.to_sym] = value
+    end
+  end
+
+  def start
+    create if ! exists?
+    @property_hash[:name]    = @resource[:name]
+    @property_flush['state'] = :Started
+    @property_hash[:ensure]  = 'started'
+  end
+
+  def stop
+    create if ! exists?
+    @property_hash[:name]    = @resource[:name]
+    @property_flush['state'] = :Stopped
+    @property_hash[:ensure]  = 'stopped'
+  end
+
+  def restart
+    inst_cmd = [
+      'Import-Module WebAdministration',
+      "Stop-WebSite -Name \"#{@resource[:name]}\"",
+      "Start-WebSite -Name \"#{@resource[:name]}\""
+    ]
+    resp = Puppet::Type::Iis_site::ProviderPowershell.run(inst_cmd.join('; '))
+    fail(resp) if resp.length > 0
+  end
+
+  def enabled?
+    inst_cmd = "Import-Module WebAdministration; (Get-WebSiteState -Name \"#{@resource[:name]}\").value"
+    resp = Puppet::Type::Iis_site::ProviderPowershell.run(inst_cmd).rstrip
+    if resp == 'Started'
+      return true
+    else
+      return false
+    end
+  end
+
+  def flush
+    command_array = []
+    command_array << "Import-Module WebAdministration; "
+    if @property_flush['state']
+      if @property_flush['state'] == :Started
+        state_cmd = 'Start-Website'
+      else
+        state_cmd = 'Stop-Website'
+      end
+      state_cmd += " -Name \"#{@property_hash[:name]}\""
+      command_array << state_cmd
+    end
+    @property_flush['itemproperty'].each do |iisname,value|
+      command_array << "Set-ItemProperty -Path \"IIS:\\\\Sites\\#{@property_hash[:name]}\" -Name \"#{iisname}\" -Value \"#{value}\""
+    end
+    bhash = {}
+    if ! @property_flush['binders'].empty?
+      Puppet::Type::Iis_site::ProviderPowershell.binders.each do |b|
+        if @property_flush['binders'].has_key?(b)
+          bhash[b] = @property_flush['binders'][b] unless @property_flush['binders'][b] == 'false'
+        else
+          bhash[b] = @property_hash[b.to_sym]
+        end
+      end
+      binder_cmd = "Set-ItemProperty -Path \"IIS:\\\\Sites\\#{@property_hash[:name]}\" -Name Bindings -Value @{protocol=\"#{bhash['protocol']}\";bindingInformation=\"#{bhash['ip']}:#{bhash['port']}:#{bhash['host_header']}"
+      binder_cmd += '"'
+      # Append sslFlags to args is enabled
+      binder_cmd += '; sslFlags=0' if bhash['ssl'] and bhash['ssl'] != :false
+      binder_cmd += '}'
+      command_array << binder_cmd
+    end
+    resp = Puppet::Type::Iis_site::ProviderPowershell.run(command_array.join('; '))
+    fail(resp) if resp.length > 0
+  end
+
+end
---
+++ lib/puppet/type/iis_site.rb
@@ -0,0 +1,113 @@
+Puppet::Type.newtype(:iis_site) do
+  desc 'The iis_site type creates and manages IIS Web Sites'
+
+  newproperty(:ensure) do
+    desc "Whether a site should be started."
+
+    newvalue(:stopped) do
+      provider.stop
+    end
+
+    newvalue(:started) do
+      provider.start
+    end
+
+    newvalue(:present) do
+      provider.create
+    end
+
+    newvalue(:absent) do
+      provider.destroy
+    end
+
+    aliasvalue(:false, :stopped)
+    aliasvalue(:true, :started)
+  end
+
+  newparam(:name, :namevar => true) do
+    desc 'This is the name of the web site'
+    validate do |value|
+      fail("#{name} is not a valid web site name") unless value =~ /^[a-zA-Z0-9\-\_'\s]+$/
+    end
+  end
+
+  newproperty(:path) do
+    desc 'Path to the web site folder'
+    validate do |value|
+      fail("File paths must be fully qualified, not '#{value}'") unless value =~ /^.:\// or value =~ /^\/\/[^\/]+\/[^\/]+/
+    end
+  end
+
+  newproperty(:app_pool) do
+    desc 'Application pool for the site'
+    validate do |value|
+      fail("#{app_pool} is not a valid application pool name") unless value =~ /^[a-zA-Z0-9\-\_'\s]+$/
+    end
+    defaultto :DefaultAppPool
+  end
+
+  newproperty(:host_header) do
+    desc 'Host header for the site'
+    validate do |value|
+      fail("#{host_header} is not a valid application pool name") unless value =~ /^[a-zA-Z0-9\-\_'\s]+$/ or value == :false
+    end
+  end
+
+  newproperty(:protocol) do
+    desc 'Protocol for the site'
+    validate do |value|
+      fail("#{protcol} is not a valid application pool name") unless value =~ /^[a-z]+$/
+    end
+  end
+
+  newproperty(:ip) do
+    desc 'IP Address for the web site'
+    def valid_v4?(addr)
+      if /^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$/ =~ addr
+        return $~.captures.all? {|i| i = i.to_i; i >= 0 and i <= 255 }
+      end
+      return false
+    end
+    def valid_v6?(addr)
+      # http://forums.dartware.com/viewtopic.php?t=452
+      # ...and, yes, it is this hard.  Doing it programatically is harder.
+      return true if addr =~ /^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$/
+
+      return false
+    end
+    validate do |value|
+      fail("Invalid IP address #{value.inspect}") unless valid_v4?(value) or valid_v6?(value) or value == '*'
+    end
+    defaultto '*'
+  end
+
+ newproperty(:port) do
+    desc 'Port the web site listens on'
+    munge do |value|
+      value.to_i
+    end
+    validate do |value|
+      #fail('Port must be an integer') unless value =~ /\d+/
+    end
+    defaultto 80
+  end
+
+  newproperty(:ssl) do
+    desc 'If ssl is enabled for the site'
+    newvalues(:false, :true)
+    defaultto :false
+  end
+
+   autorequire(:iis_pool) do
+    self[:app_pool] if @parameters.include? :app_pool
+  end 
+
+  def refresh
+    if self[:ensure] == :present and (provider.enabled? or self[:ensure] == 'started')
+      provider.restart
+    else
+      debug "Skipping restart; site is not running"
+    end
+  end
+
+end
