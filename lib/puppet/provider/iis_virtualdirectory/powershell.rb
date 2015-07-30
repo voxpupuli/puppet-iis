f4827deda609617b6d04f8b6760c0bae5e135a2f
Travis Fields <travis@puppetlabs.com>
2015-07-30 14:12:41 -0700

    (maint) Virtual Directory work, fix issue with assuming array

--- lib/puppet/provider/iis_virtualdirectory/powershell.rb
+++ lib/puppet/provider/iis_virtualdirectory/powershell.rb
@@ -6,28 +6,34 @@ Puppet::Type.type(:iis_virtualdirectory).provide(:powershell, :parent => Puppet:
   def initialize(value={})
     super(value)
     @property_flush = {
-      'vdattrs' => {},
+        'vdattrs' => {},
     }
   end
 
   def self.instances
-    inst_cmd = 'Import-Module WebAdministration; Get-WebVirtualDirectory | Select path, physicalPath, ItemXPath | ConvertTo-JSON'
-    vd_names = JSON.parse(run(inst_cmd))
-    vd_names = [vd_names] if vd_names.is_a?(Hash)
-    vd_names.collect do |vd|
-      vd_hash          = {}
-      vd_hash[:name]   = vd['path'].gsub(/^\//, '')
-      vd_hash[:path]   = vd['physicalPath']
-      vd_hash[:site]   = vd['ItemXPath'].match(/@name='([a-z0-9_\ ]+)'/i)[1]
-      vd_hash[:ensure] = :present
-      new(vd_hash)
+    virtual_directories = []
+    inst_cmd = 'Import-Module WebAdministration; Get-WebVirtualDirectory | Select path, physicalPath, ItemXPath | ConvertTo-JSON -Depth 4'
+    result = run(inst_cmd)
+    if !result.empty?
+      vd_names = JSON.parse(result)
+      vd_names = [vd_names] if vd_names.is_a?(Hash)
+      vd_names.each do |vd|
+        vd_hash = {}
+        vd_hash[:name] = vd['path'].gsub(/^\//, '')
+        vd_hash[:path] = vd['physicalPath']
+        vd_hash[:site] = vd['ItemXPath'].match(/@name='([a-z0-9_\ ]+)'/i)[1]
+        vd_hash[:ensure] = :present
+        virtual_directories << new(vd_hash)
+      end
     end
+
+    virtual_directories
   end
 
   def self.prefetch(resources)
     vds = instances
     resources.keys.each do |vd|
-      if provider = vds.find{ |v| v.name == vd }
+      if provider = vds.find { |v| v.name == vd }
         resources[vd].provider = provider
       end
     end
@@ -41,11 +47,11 @@ Puppet::Type.type(:iis_virtualdirectory).provide(:powershell, :parent => Puppet:
 
   def create
     inst_cmd = [
-      'Import-Module WebAdministration; ',
-      "New-WebVirtualDirectory -Name \"#{@resource[:name]}\"",
-      "-PhysicalPath \"#{@resource[:path]}\"",
-      "-Site \"#{@resource[:site]}\"",
-      '-Force'
+        'Import-Module WebAdministration; ',
+        "New-WebVirtualDirectory -Name \"#{@resource[:name]}\"",
+        "-PhysicalPath \"#{@resource[:path]}\"",
+        "-Site \"#{@resource[:site]}\"",
+        '-Force'
     ]
     resp = Puppet::Type::Iis_virtualdirectory::ProviderPowershell.run(inst_cmd.join(' '))
 
@@ -59,10 +65,10 @@ Puppet::Type.type(:iis_virtualdirectory).provide(:powershell, :parent => Puppet:
 
   def destroy
     inst_cmd = [
-      'Import-Module WebAdministration; ',
-      'Remove-Item',
-      "\"IIS:\\Sites\\#{@property_hash[:site]}\\#{@property_hash[:name]}\"",
-      '-Force -Recurse'
+        'Import-Module WebAdministration; ',
+        'Remove-Item',
+        "\"IIS:\\Sites\\#{@property_hash[:site]}\\#{@property_hash[:name]}\"",
+        '-Force -Recurse'
     ]
     resp = Puppet::Type::Iis_virtualdirectory::ProviderPowershell.run(inst_cmd.join(' '))
     fail(resp) if resp.length > 0
@@ -83,7 +89,7 @@ Puppet::Type.type(:iis_virtualdirectory).provide(:powershell, :parent => Puppet:
   def flush
     command_array = []
     command_array << "Import-Module WebAdministration"
-    @property_flush['vdattrs'].each do |vdattr,value|
+    @property_flush['vdattrs'].each do |vdattr, value|
       command_array << "Set-ItemProperty \"IIS:\\\\Sites\\#{@property_hash[:site]}\\#{@property_hash[:name]}\" #{vdattr} #{value}"
     end
     resp = Puppet::Type::Iis_virtualdirectory::ProviderPowershell.run(command_array.join('; '))
