3247cd341cb36b223a0d2f16c1576b5afa813f6c
Travis Fields <travis@puppetlabs.com>
2015-07-30 17:15:13 -0700

    (tests) Add iis_pool spec tests

--- spec/unit/puppet/type/iis_pool_spec.rb
+++ spec/unit/puppet/type/iis_pool_spec.rb
@@ -1,36 +1,53 @@
 require 'puppet'
 require 'puppet/type/iis_pool'
+require File.expand_path(File.join(File.dirname(__FILE__), 'iis_stateful_shared_examples.rb'))
 
 describe Puppet::Type.type(:iis_pool) do
-
-  before :each do
-    @pool = Puppet::Type.type(:iis_pool).new(
-      :name          => 'test_pool',
-      :ensure        => 'started',
+  let(:params) { {
+      :name => 'test_pool',
+      :ensure => 'started',
       :enable_32_bit => true,
-      :runtime       => '4.0',
-      :pipeline      => 'Classic',
-    )
+      :runtime => '4.0',
+      :pipeline => 'Classic',
+  } }
+
+  def subject(p = params)
+    Puppet::Type.type(:iis_pool).new(p)
   end
 
   it 'should accept a pool name' do
-    expect(@pool[:name]).to eq('test_pool')
+    expect(subject[:name]).to eq('test_pool')
   end
 
-  it 'should accept an ensure state' do
-    expect(@pool[:ensure]).to eq(:started)
+  describe 'ensure' do
+    include_context 'stateful types'
+    it_behaves_like 'stateful type'
   end
 
   it 'should accept an enable_32_bit state' do
-    expect(@pool[:enable_32_bit]).to eq(:true)
+    expect(subject[:enable_32_bit]).to eq(:true)
   end
 
-  it 'should accept a runtime' do
-    expect(@pool[:runtime]).to eq('v4.0')
+  describe 'runtime =>' do
+    it 'should accept a runtime' do
+      expect(subject[:runtime]).to eq('v4.0')
+    end
+    it 'should munge runtime' do
+      expect(subject(params.merge({:runtime => '4.0'}))[:runtime]).to eq('v4.0')
+    end
   end
 
-  it 'should accept a pipeline' do
-    expect(@pool[:pipeline]).to eq('Classic')
+  describe 'pipeline =>' do
+    ['Integrated', 'Classic'].each do |pipeline|
+      it "should accept #{pipeline}" do
+        param = params.merge({:pipeline => pipeline})
+        expect(subject(param)[:pipeline]).to eq(pipeline)
+      end
+      it "should munge #{pipeline.downcase} to capital" do
+        param = params.merge({:pipeline => pipeline.downcase})
+        expect(subject(param)[:pipeline]).to eq(pipeline)
+      end
+    end
   end
 
 end
