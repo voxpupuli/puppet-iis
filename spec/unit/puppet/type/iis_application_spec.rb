56efee67bb2c530ad5ba0529c3a9caf36fde4fb9
Travis Fields <travis@puppetlabs.com>
2015-07-30 17:09:29 -0700

    (tests) Add more unit tests for iis_application

--- spec/unit/puppet/type/iis_application_spec.rb
+++ spec/unit/puppet/type/iis_application_spec.rb
@@ -1,36 +1,56 @@
 require 'puppet'
 require 'puppet/type/iis_application'
+require File.expand_path(File.join(File.dirname(__FILE__), 'iis_stateful_shared_examples.rb'))
 
 describe Puppet::Type.type(:iis_application) do
+  let(:params) { {
+      :name => 'test_application',
+      :ensure => 'started',
+      :path => 'C:/Temp',
+      :site => 'Default Web Site',
+      :app_pool => 'DefaultAppPool',
+  } }
 
-  before :each do
-    @application = Puppet::Type.type(:iis_application).new(
-      :name          => 'test_application',
-      :ensure        => 'started',
-      :path          => 'C:/Temp',
-      :site          => 'Default Web Site',
-      :app_pool      => 'DefaultAppPool',
-    )
+  def subject(p = params)
+    Puppet::Type.type(:iis_application).new(p)
   end
 
-  it 'should accept a application name' do
-    expect(@application[:name]).to eq('test_application')
+  describe 'name =>' do
+    it 'should accept underscore' do
+      expect(subject[:name]).to eq('test_application')
+    end
+    it 'should accept uppercase' do
+      expect(subject(params.merge({:name => 'MyApplication'}))[:name]).to eq('MyApplication')
+    end
   end
 
-  it 'should accept an ensure state' do
-    expect(@application[:ensure]).to eq(:started)
+  describe 'ensure =>' do
+    include_context 'stateful types'
+    it_behaves_like 'stateful type'
+  end
+  describe 'path =>' do
+    it 'should accept forwardslash' do
+      expect(subject[:path]).to eq('C:/Temp')
+    end
+    it 'should accept backslash' do
+      expect(subject(params.merge({:path => 'C:\Temp'}))[:path]).to eq('C:\Temp')
+    end
+    it 'should reject network' do
+      expect { subject(params.merge({:path => '//remote/Temp'}))[:path] }.to raise_error
+    end
   end
 
-  it 'should accept a path' do
-    expect(@application[:path]).to eq('C:/Temp')
-  end
-
-  it 'should accept a site' do
-    expect(@application[:site]).to eq('Default Web Site')
+  describe 'site =>' do
+    it 'should accept default site' do
+      expect(subject[:site]).to eq('Default Web Site')
+    end
+    it 'should accept non default site' do
+      expect(subject(params.merge({:site => 'MySite'}))[:site]).to eq('MySite')
+    end
   end
 
   it 'should accept an app_pool' do
-    expect(@application[:app_pool]).to eq('DefaultAppPool')
+    expect(subject[:app_pool]).to eq('DefaultAppPool')
   end
 
 end
