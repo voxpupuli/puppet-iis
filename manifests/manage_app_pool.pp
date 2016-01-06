4e4755e2a45dc4b5e76d3b365589418f34090c3e
lc8158 <lc8158@LCOULSON01D.intdom.bna.com>
2016-01-06 12:25:06 -0500

    manage_app_pool - add periodic recycle control
    new param left default undef
    logic in class handles undef
    
    caller set to 0 to disable periodic app pool recycling
    
    minutes value converted to ticks and given to powershell for timespan
    constructor as int64 max value is powerhsell $([int64]::MaxValue)
    
    validation will reject negatives or out of max bound

--- manifests/manage_app_pool.pp
+++ manifests/manage_app_pool.pp
@@ -1,20 +1,27 @@
-#
-define iis::manage_app_pool(
+#apppool periodic cycle - if want to disable app pool cycling on periodic time set $apppoolperiodicrecycle to 0 - it's minutes
+define iis::manage_app_pool (
   $app_pool_name           = $title,
   $enable_32_bit           = false,
   $managed_runtime_version = 'v4.0',
   $managed_pipeline_mode   = 'Integrated',
   $ensure                  = 'present',
   $start_mode              = 'OnDemand',
-  $rapid_fail_protection   = true
-){
-
+  $rapid_fail_protection   = true,
+  $apppoolrecycleperiodicminutes = undef
+  ) {
   validate_bool($enable_32_bit)
   validate_re($managed_runtime_version, ['^(v2\.0|v4\.0)$'])
   validate_re($managed_pipeline_mode, ['^(Integrated|Classic)$'])
   validate_re($ensure, '^(present|installed|absent|purged)$', 'ensure must be one of \'present\', \'installed\', \'absent\', \'purged\'')
   validate_re($start_mode, '^(OnDemand|AlwaysRunning)$')
   validate_bool($rapid_fail_protection)
+
+if $apppoolrecycleperiodicminutes != undef {
+    validate_integer($apppoolrecycleperiodicminutes, 15372286728, 0) # powershell $([int64]::MaxValue) / 600000000, we're not dealing with negative
+    $periodicticks = $apppoolrecycleperiodicminutes * 600000000
+    $processperiodictimes = true
+}
+else{$processperiodictimes = false}
 
   if ($ensure in ['present','installed']) {
     exec { "Create-${app_pool_name}" :
@@ -23,7 +30,7 @@ define iis::manage_app_pool(
       onlyif    => "Import-Module WebAdministration; if((Test-Path \"IIS:\\AppPools\\${app_pool_name}\")) { exit 1 } else { exit 0 }",
       logoutput => true,
     }
-    
+
     exec { "StartMode-${app_pool_name}" :
       command   => "Import-Module WebAdministration; Set-ItemProperty \"IIS:\\AppPools\\${app_pool_name}\" startMode ${start_mode}",
       provider  => powershell,
@@ -39,7 +46,7 @@ define iis::manage_app_pool(
       require   => Exec["Create-${app_pool_name}"],
       logoutput => true,
     }
-    
+
     exec { "Framework-${app_pool_name}" :
       command   => "Import-Module WebAdministration; Set-ItemProperty \"IIS:\\AppPools\\${app_pool_name}\" managedRuntimeVersion ${managed_runtime_version}",
       provider  => powershell,
@@ -69,8 +76,21 @@ define iis::manage_app_pool(
       require   => Exec["Create-${app_pool_name}"],
       logoutput => true,
     }
-  } else {
-    exec { "Delete-${app_pool_name}" :
+
+        if($processperiodictimes)
+    {
+        exec { "App Pool Recycle Periodic - ${app_pool_name} - ${apppoolrecycleperiodicminutes}":
+        command   => "\$appPoolName = \"${app_pool_name}\";[TimeSpan] \$ts = ${periodicticks};Import-Module WebAdministration;\$appPoolPath = (\"IIS:\\AppPools\\\" + \$appPoolName);Get-ItemProperty \$appPoolPath -Name recycling.periodicRestart.time;Set-ItemProperty \$appPoolPath -Name recycling.periodicRestart.time -value \$ts;",
+        provider  => powershell,
+        unless    => "\$appPoolName = \"${app_pool_name}\";[TimeSpan] \$ts = ${periodicticks};Import-Module WebAdministration;\$appPoolPath = (\"IIS:\\AppPools\\\" + \$appPoolName);if((Get-ItemProperty \$appPoolPath -Name recycling.periodicRestart.time.value) -ne \$ts.Ticks){exit 1;}exit 0;",
+        require   => Exec["Create-${app_pool_name}"],
+        logoutput => true,
+      }
+    }
+
+    }
+ else {
+    exec { "Delete-${app_pool_name}":
       command   => "Import-Module WebAdministration; Remove-Item \"IIS:\\AppPools\\${app_pool_name}\" -Recurse",
       provider  => powershell,
       onlyif    => "Import-Module WebAdministration; if(!(Test-Path \"IIS:\\AppPools\\${app_pool_name}\")) { exit 1 } else { exit 0 }",
